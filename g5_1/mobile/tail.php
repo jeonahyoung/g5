608fea039b16071ff642430afaae71dff52e61f9
jeonahyoung <a40145061@gmail.com>
2025-07-08 15:29:55 +0900

    upload
    upload

--- g5_1/mobile/tail.php
+++ g5_1/mobile/tail.php
@@ -10,13 +10,23 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
             
 
             <div id="ft_company">
-                <a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=company">회사소개</a>
-                <a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=privacy">개인정보처리방침</a>
-                <a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=provision">서비스이용약관</a>
-                <p>주소: 서울특별시 종로구 종로2가 71-2</p>
-                <p>사업자번호: 221-45-87943</p>
+                <a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=company">환자권리장전</a>
+                <a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=privacy">비급여진료비</a>
+                <a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=provision">개인정보취급방침</a>
+                <a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=use">이용약관</a>
+                <a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=store">제휴협력</a>
+                <a href="<?php echo G5_BBS_URL; ?>/content.php?co_id=consumer">고객의소리</a>
+                <ul class="footer-list">
+                    <li class="footer-ceo">대표자: 전아영</li>
+                    <li>사업자번호: 221-45-87943</li>
+                    <li>상호명: 샤인안과</li>
+                    <li>대표전화: 1522-0774</li>
+                </ul>
+                <p>서울특별시 종로구 종로2가 71-2</p>
+                <br>
+                <p class="footer-copy">Copyright &copy; <b>http://jay230.dothome.co.kr/</b> All rights reserved.</p>
             </div>
-            <div id="ft_copy">Copyright &copy; <b>http://jay230.dothome.co.kr/</b> All rights reserved.</div>
+            <!-- <div id="ft_copy">Copyright &copy; <b>http://jay230.dothome.co.kr/</b> All rights reserved.</div> -->
 
     </div>
     <button type="button" id="top_btn"><i class="fa fa-arrow-up" aria-hidden="true"></i><span class="sound_only">상단으로</span></button>
